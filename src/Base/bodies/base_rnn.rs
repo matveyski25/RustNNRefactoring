12af8f570df64cf91d6c8ccaece848e2b3010978
Remi <matveyski25@gmail.com>
2026-03-16 10:07:33 +0300

    second
    Signed-off-by: Remi <matveyski25@gmail.com>

--- src/Base/bodies/base_rnn.rs
+++ src/Base/bodies/base_rnn.rs
@@ -1,3 +1,4 @@
+use derive_more::Deref;
 use num_traits::Num;
 
 use crate::Base::{
@@ -5,24 +6,29 @@ use crate::Base::{
     interfaces::base_nn::{ComputeBlock, Optimizer, Randomizer, SaveManager, TranslatorMatrix},
 };
 
+#[derive(Clone, Deref)]
 pub struct BodyComputeBlockRNN<T: Num> {
+    #[deref]
     pub body_compute_block: BodyComputeBlock<T>,
     pub hidden_size: u64,
     pub max_steps: u64,
 }
 
+#[derive(Clone, Deref)]
 pub struct BodyTrainableComputeBlockRNN<T: Num, Ra: Randomizer, Op: Optimizer> {
     pub body_compute_block_rnn: BodyComputeBlockRNN<T>,
     pub body_trainable_compute_block: BodyTrainableComputeBlock<T, Ra, Op>,
 }
 
+#[derive(Clone, Deref)]
 pub struct BodyBaseRNN<T: Num, SM: SaveManager, CB: ComputeBlock<T>, Tr: TranslatorMatrix<T>>(
     BodyBaseNN<T, SM, CB, Tr>,
 ); //body_base_nn:
 
+#[derive(Clone, Deref)]
 pub struct BodyBaseTrainableRNN<
     T: Num,
     SM: SaveManager,
     CB: ComputeBlock<T>,
     Tr: TranslatorMatrix<T>,
->(BodyBaseRNN<T, SM, CB, Tr>); //body_base_rnn: 
+>(BodyBaseRNN<T, SM, CB, Tr>); //body_base_rnn:
